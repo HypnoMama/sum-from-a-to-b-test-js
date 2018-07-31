4ab538c99bf9cb6be1084530ac4fc0696fe54c17
HypnoMama <sarahhermsen@gmail.com>
2018-07-31 23:16:56 +0000

    wrote recursive function

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN === toN){
+    return toN;
+  } else {
+    return fromN + sum(fromN + 1, toN)
+  }
 }
 
 module.exports = sum;
+
+// from: 5 to: 10
+
+// should be 45
+
